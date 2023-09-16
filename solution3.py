5d69c940217a82c96a837867bc0fb94d99126696
“JonsCodes” <johnalyashai@me.com>
2023-09-16 12:16:57 -0700

    Completed code

--- solution3.py
+++ solution3.py
@@ -1,12 +1,9 @@
-# Write code for algorithm 3 below
-
-def fibonacci(n):
-    if n < 0:
-        return 0
-    elif n == 1:
-        return 1
+def fibonacci_recursive(n):
+    if n <= 1:
+        return n
     else:
-        return fibonacci(n-2) + fibonacci(n-1)
-    # Test the function
-    print(fibonacci(5)) # Output: 2
-    print(fibonacci(2)) # Output: 1
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+
+# Test
+print(fibonacci_recursive(4))  # Output: 3
+print(fibonacci_recursive(2))  # Output: 1
